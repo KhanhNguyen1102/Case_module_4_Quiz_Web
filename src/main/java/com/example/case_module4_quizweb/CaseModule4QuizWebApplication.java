ab25d96109c96a77f3300ba9cd6aa380a9011607
KhanhNguyen1102 <sukhanh12@gmail.com>
2022-01-02 23:54:38 +0700

    first_commit

---
+++ src/main/java/com/example/case_module4_quizweb/CaseModule4QuizWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.case_module4_quizweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseModule4QuizWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseModule4QuizWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/case_module4_quizweb/CaseModule4QuizWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.case_module4_quizweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseModule4QuizWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
