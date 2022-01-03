ab1a514e4e2c9adccba0ad9fc9ed5b3a358af5fc
quanghuy2002 <hoangqunghuy753@gmail.com>
2022-01-03 14:33:06 +0700

    add

--- src/main/java/com/example/case_module4_quizweb/controller/UserController.java
+++ src/main/java/com/example/case_module4_quizweb/controller/UserController.java
@@ -5,11 +5,9 @@ import com.example.case_module4_quizweb.service.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -27,5 +25,13 @@ public class UserController {
         }
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
+
+    @PostMapping("/save")
+    public ResponseEntity<User> save(@RequestBody User user) {
+        userService.save(user);
+        return new ResponseEntity<>( HttpStatus.CREATED);
+    }
+
+
 
 }
