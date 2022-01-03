e2ffd5297c065fade08dc329415bd3bca00f43d0
hoangducviet1993 <ducviet12vl@gmail.com>
2022-01-03 12:33:34 +0700

    done edit QUiz

--- src/main/java/com/example/case_module4_quizweb/controller/QuizController.java
+++ src/main/java/com/example/case_module4_quizweb/controller/QuizController.java
@@ -19,7 +19,7 @@ public class QuizController {
     private IQuizService quizService;
 
     @GetMapping("")
-    public ResponseEntity<Iterable<Quiz>> findAllCateGory() {
+    public ResponseEntity<Iterable<Quiz>> findAllQuiz() {
         List<Quiz> quizzes = (List<Quiz>) quizService.findAll();
         if (quizzes.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
@@ -27,7 +27,7 @@ public class QuizController {
         return new ResponseEntity<>(quizzes, HttpStatus.OK);
     }
     @PostMapping("")
-    public ResponseEntity<Quiz> findCategoryById(@RequestParam Long id) {
+    public ResponseEntity<Quiz> findQuizById(@RequestParam Long id) {
         Optional<Quiz> quizOptional = quizService.findById(id);
         if (!quizOptional.isPresent()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -35,4 +35,28 @@ public class QuizController {
         Quiz quiz = quizOptional.get();
         return new ResponseEntity<>(quiz, HttpStatus.OK);
     }
+    @PostMapping("/create")
+    public ResponseEntity<Quiz> createQuiz(@RequestBody Quiz quiz){
+        return new ResponseEntity<>(quizService.save(quiz), HttpStatus.OK);
+    }
+    @PutMapping("")
+    public ResponseEntity<Quiz> updateQuiz(@RequestParam Long id, @RequestBody Quiz quiz) {
+        Optional<Quiz> quizOptional = quizService.findById(id);
+        if (!quizOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        quiz.setId(quizOptional.get().getId());
+
+        return new ResponseEntity<>(quizService.save(quiz), HttpStatus.OK);
+    }
+//
+//    @DeleteMapping("")
+//    public ResponseEntity<Quiz> deleteQuiz(@RequestParam Long id) {
+//        Optional<Quiz> quizOptional = quizService.findById(id);
+//        if (!quizOptional.isPresent()) {
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        }
+//        quizService.remove(id);
+//        return new ResponseEntity<>(quizOptional.get(), HttpStatus.NO_CONTENT);
+//    }
 }
