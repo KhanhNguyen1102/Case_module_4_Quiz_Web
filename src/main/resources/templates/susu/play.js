e397050c212b271efe9a91720748194eed9adac7
KhanhNguyen1102 <sukhanh12@gmail.com>
2022-01-10 23:03:47 +0700

    load_correct_answer

--- src/main/resources/templates/susu/play.js
+++ src/main/resources/templates/susu/play.js
@@ -6,6 +6,16 @@ function playTest() {
         headers: {"Authorization": 'Bearer ' + localStorage.getItem("token")},
         success: function (test) {
             console.log(test)
+            localStorage.setItem("score1",test.quizzes[0].value);
+            localStorage.setItem("score2",test.quizzes[1].value);
+            localStorage.setItem("score3",test.quizzes[2].value);
+            localStorage.setItem("score4",test.quizzes[3].value);
+            localStorage.setItem("score5",test.quizzes[4].value);
+            localStorage.setItem("score6",test.quizzes[5].value);
+            localStorage.setItem("score7",test.quizzes[6].value);
+            localStorage.setItem("score8",test.quizzes[7].value);
+            localStorage.setItem("score9",test.quizzes[8].value);
+            localStorage.setItem("score10",test.quizzes[9].value);
             localStorage.setItem("content1", test.quizzes[0].content);
             localStorage.setItem("content2", test.quizzes[1].content);
             localStorage.setItem("content3", test.quizzes[2].content);
@@ -26,6 +36,46 @@ function playTest() {
             localStorage.setItem("quizId8", test.quizzes[7].id);
             localStorage.setItem("quizId9", test.quizzes[8].id);
             localStorage.setItem("quizId10", test.quizzes[9].id);
+            localStorage.setItem("choice11","1");
+            localStorage.setItem("choice12","1");
+            localStorage.setItem("choice13","1");
+            localStorage.setItem("choice14","1");
+            localStorage.setItem("choice21","1");
+            localStorage.setItem("choice22","1");
+            localStorage.setItem("choice23","1");
+            localStorage.setItem("choice24","1");
+            localStorage.setItem("choice31","1");
+            localStorage.setItem("choice32","1");
+            localStorage.setItem("choice33","1");
+            localStorage.setItem("choice34","1");
+            localStorage.setItem("choice41","1");
+            localStorage.setItem("choice42","1");
+            localStorage.setItem("choice43","1");
+            localStorage.setItem("choice44","1");
+            localStorage.setItem("choice51","1");
+            localStorage.setItem("choice52","1");
+            localStorage.setItem("choice53","1");
+            localStorage.setItem("choice54","1");
+            localStorage.setItem("choice61","1");
+            localStorage.setItem("choice62","1");
+            localStorage.setItem("choice63","1");
+            localStorage.setItem("choice64","1");
+            localStorage.setItem("choice71","1");
+            localStorage.setItem("choice72","1");
+            localStorage.setItem("choice73","1");
+            localStorage.setItem("choice74","1");
+            localStorage.setItem("choice81","1");
+            localStorage.setItem("choice82","1");
+            localStorage.setItem("choice83","1");
+            localStorage.setItem("choice84","1");
+            localStorage.setItem("choice91","1");
+            localStorage.setItem("choice92","1");
+            localStorage.setItem("choice93","1");
+            localStorage.setItem("choice94","1");
+            localStorage.setItem("choice101","1");
+            localStorage.setItem("choice102","1");
+            localStorage.setItem("choice103","1");
+            localStorage.setItem("choice104","1");
             let i = 2;
             playingTest(test.quizzes[0].content, test.quizzes[0].id, i);
 
@@ -42,6 +92,15 @@ function playingTest(test, quizId, i) {
         let previousQuiz = localStorage.getItem("quizId" + (i-2));
         localStorage.setItem("nextContent",nextContent);
         localStorage.setItem("previousContent",previousContent);
+        localStorage.setItem("nowContent",test);
+        let choice1 = localStorage.getItem("choice"+(i-1)+"1");
+        let choice2 = localStorage.getItem("choice"+(i-1)+"2");
+        let choice3 = localStorage.getItem("choice"+(i-1)+"3");
+        let choice4 = localStorage.getItem("choice"+(i-1)+"4");
+        console.log(choice1);
+        console.log(choice2);
+        console.log(choice3);
+        console.log(choice4);
         $.ajax({
             type: "GET",
             url: "http://localhost:8080/api/answers/quiz?id=" + quizId,
@@ -49,6 +108,13 @@ function playingTest(test, quizId, i) {
             success: function (data) {
                 console.log(nextContent);
                 console.log(nextQuiz);
+                for (let j = 0; j < data.length; j++) {
+                    if (data[j].correct === 1){
+                        // localStorage.setItem("correct"+(i-1),"choice"+(i-1)+(j+1));
+                        localStorage.setItem("correct"+(i-1),"choice"+(i-1)+(j+1));
+                        break;
+                    }
+                }
                 let form = `<div class="row">
         <div class="col-12">
             <div class="row">
@@ -62,12 +128,12 @@ function playingTest(test, quizId, i) {
             </button>
         </div>
         <div class="col-12">
-            <button id="button2">${data[0].content}</button>
-            <button id="button3">${data[1].content}</button>
+            <button id="button2" onclick="pickChoice1('${localStorage.getItem('nowContent')}',${quizId},${i})">${data[0].content}</button>
+            <button id="button3" onclick="pickChoice2('${localStorage.getItem('nowContent')}',${quizId},${i})">${data[1].content}</button>
         </div>
         <div class="col-12">
-            <button id="button4">${data[2].content}</button>
-            <button id="button5">${data[3].content}</button>
+            <button id="button4" onclick="pickChoice3('${localStorage.getItem('nowContent')}',${quizId},${i})">${data[2].content}</button>
+            <button id="button5" onclick="pickChoice4('${localStorage.getItem('nowContent')}',${quizId},${i})">${data[3].content}</button>
         </div>
         <div class="col-12">
             <div class="row">
@@ -76,14 +142,30 @@ function playingTest(test, quizId, i) {
             </div>
         </div>
     </div>`
-
                 document.getElementById("content").innerHTML = form;
+                if (choice1 === "2"){
+                    document.getElementById("button2").style.background = '#f7797d';
+                }
+                if (choice2 === "2"){
+                    document.getElementById("button3").style.background = '#f7797d';
+                }
+                if (choice3 === "2"){
+                    document.getElementById("button4").style.background = '#f7797d';
+                }
+                if (choice4 === "2"){
+                    document.getElementById("button5").style.background = '#f7797d';
+                }
             }
         })
     }else if (i===2){
         let nextContent = localStorage.getItem("content" + i);
         let nextQuiz = localStorage.getItem("quizId" + i);
         localStorage.setItem("nextContent",nextContent);
+        localStorage.setItem("nowContent",test);
+        let choice1 = localStorage.getItem("choice"+(i-1)+"1");
+        let choice2 = localStorage.getItem("choice"+(i-1)+"2");
+        let choice3 = localStorage.getItem("choice"+(i-1)+"3");
+        let choice4 = localStorage.getItem("choice"+(i-1)+"4");
         $.ajax({
             type: "GET",
             url: "http://localhost:8080/api/answers/quiz?id=" + quizId,
@@ -91,6 +173,12 @@ function playingTest(test, quizId, i) {
             success: function (data) {
                 console.log(nextContent);
                 console.log(nextQuiz);
+                for (let j = 0; j < data.length; j++) {
+                    if (data[j].correct === 1){
+                        localStorage.setItem("correct"+(i-1),"choice"+(i-1)+(j+1));
+                        break;
+                    }
+                }
                 let form = `<div class="row">
         <div class="col-12">
             <div class="row">
@@ -103,13 +191,13 @@ function playingTest(test, quizId, i) {
                 ${test}
             </button>
         </div>
-        <div class="col-12">
-            <button id="button2">${data[0].content}</button>
-            <button id="button3">${data[1].content}</button>
+       <div class="col-12">
+            <button id="button2" onclick="pickChoice1('${localStorage.getItem('nowContent')}',${quizId},${i})">${data[0].content}</button>
+            <button id="button3" onclick="pickChoice2('${localStorage.getItem('nowContent')}',${quizId},${i})">${data[1].content}</button>
         </div>
         <div class="col-12">
-            <button id="button4">${data[2].content}</button>
-            <button id="button5">${data[3].content}</button>
+            <button id="button4" onclick="pickChoice3('${localStorage.getItem('nowContent')}',${quizId},${i})">${data[2].content}</button>
+            <button id="button5" onclick="pickChoice4('${localStorage.getItem('nowContent')}',${quizId},${i})">${data[3].content}</button>
         </div>
         <div class="col-12">
             <div class="row">
@@ -118,17 +206,41 @@ function playingTest(test, quizId, i) {
         </div>
     </div>`
                 document.getElementById("content").innerHTML = form;
+                document.getElementById("content").innerHTML = form;
+                if (choice1 === "2"){
+                    document.getElementById("button2").style.background = '#f7797d';
+                }
+                if (choice2 === "2"){
+                    document.getElementById("button3").style.background = '#f7797d';
+                }
+                if (choice3 === "2"){
+                    document.getElementById("button4").style.background = '#f7797d';
+                }
+                if (choice4 === "2"){
+                    document.getElementById("button5").style.background = '#f7797d';
+                }
             }
         })
     }else {
         let previousContent = localStorage.getItem("content" + (i-2));
         let previousQuiz = localStorage.getItem("quizId" + (i-2));
         localStorage.setItem("previousContent",previousContent);
+        localStorage.setItem("nowContent",test);
+        let choice1 = localStorage.getItem("choice"+(i-1)+"1");
+        let choice2 = localStorage.getItem("choice"+(i-1)+"2");
+        let choice3 = localStorage.getItem("choice"+(i-1)+"3");
+        let choice4 = localStorage.getItem("choice"+(i-1)+"4");
         $.ajax({
             type: "GET",
             url: "http://localhost:8080/api/answers/quiz?id=" + quizId,
             headers: {"Authorization": 'Bearer ' + localStorage.getItem("token")},
             success: function (data) {
+                for (let j = 0; j < data.length; j++) {
+                    if (data[j].correct === 1){
+                        localStorage.setItem("correct"+(i-1),"choice"+(i-1)+(j+1));
+                        break;
+                    }
+                }
                 let form = `<div class="row">
         <div class="col-12">
             <div class="row">
@@ -138,27 +250,73 @@ function playingTest(test, quizId, i) {
         </div>
         <div class="col-12">
             <button class="button1">
-                ${test}c
+                ${test}
             </button>
         </div>
-        <div class="col-12">
-            <button id="button2">${data[0].content}</button>
-            <button id="button3">${data[1].content}</button>
+         <div class="col-12">
+            <button id="button2" onclick="pickChoice1('${localStorage.getItem('nowContent')}',${quizId},${i})">${data[0].content}</button>
+            <button id="button3" onclick="pickChoice2('${localStorage.getItem('nowContent')}',${quizId},${i})">${data[1].content}</button>
         </div>
         <div class="col-12">
-            <button id="button4">${data[2].content}</button>
-            <button id="button5">${data[3].content}</button>
+            <button id="button4" onclick="pickChoice3('${localStorage.getItem('nowContent')}',${quizId},${i})">${data[2].content}</button>
+            <button id="button5" onclick="pickChoice4('${localStorage.getItem('nowContent')}',${quizId},${i})">${data[3].content}</button>
         </div>
         <div class="col-12">
             <div class="row">
                 <button id="previous" onclick="playingTest('${localStorage.getItem('previousContent')}',${previousQuiz},${i-1})">Previous</button>
-                <button id="next" >Finish</button>
+                <button id="next" onclick="calScore()" >Finish</button>
             </div>
         </div>
     </div>`
 
                 document.getElementById("content").innerHTML = form;
+                document.getElementById("content").innerHTML = form;
+                if (choice1 === "2"){
+                    document.getElementById("button2").style.background = '#f7797d';
+                }
+                if (choice2 === "2"){
+                    document.getElementById("button3").style.background = '#f7797d';
+                }
+                if (choice3 === "2"){
+                    document.getElementById("button4").style.background = '#f7797d';
+                }
+                if (choice4 === "2"){
+                    document.getElementById("button5").style.background = '#f7797d';
+                }
             }
         })
+    }
+}
+function pickChoice1(test, quizId, i){
+    localStorage.setItem("choice"+(i-1)+"1","2");
+    localStorage.setItem("choice"+(i-1)+"2","1");
+    localStorage.setItem("choice"+(i-1)+"3","1");
+    localStorage.setItem("choice"+(i-1)+"4","1");
+    playingTest(test, quizId, i)
+}
+function pickChoice2(test, quizId, i){
+    localStorage.setItem("choice"+(i-1)+"1","1");
+    localStorage.setItem("choice"+(i-1)+"2","2");
+    localStorage.setItem("choice"+(i-1)+"3","1");
+    localStorage.setItem("choice"+(i-1)+"4","1");
+    playingTest(test, quizId, i)
+}
+function pickChoice3(test, quizId, i){
+    localStorage.setItem("choice"+(i-1)+"1","1");
+    localStorage.setItem("choice"+(i-1)+"2","1");
+    localStorage.setItem("choice"+(i-1)+"3","2");
+    localStorage.setItem("choice"+(i-1)+"4","1");
+    playingTest(test, quizId, i)
+}
+function pickChoice4(test, quizId, i){
+    localStorage.setItem("choice"+(i-1)+"1","1");
+    localStorage.setItem("choice"+(i-1)+"2","1");
+    localStorage.setItem("choice"+(i-1)+"3","1");
+    localStorage.setItem("choice"+(i-1)+"4","2");
+    playingTest(test, quizId, i)
+}
+function calScore(){
+    for (let k = 1; k <11 ; k++) {
+
     }
 }
